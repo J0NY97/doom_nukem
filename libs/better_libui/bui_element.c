34e531e425fc795cc38b237fc4df545b6cd9d6de
J0NY97 <jony.salmi97@gmail.com>
2021-07-26 18:06:18 +0300

    strdup font name

--- libs/better_libui/bui_element.c
+++ libs/better_libui/bui_element.c
@@ -201,7 +201,7 @@ void	bui_change_element_text(t_bui_element *elem, char *text)
 {
 	char	*font_name;
 
-	font_name = elem->font_name;
+	font_name = ft_strdup(elem->font_name);
 	ft_strdel(&elem->text);
 	elem->text = ft_strdup(text);
 	bui_set_element_text_font(elem, font_name, elem->font_size, elem->text_color);
