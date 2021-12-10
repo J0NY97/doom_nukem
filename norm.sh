822cbeaa0b98874299595da3ff0e6a0025bc2509
Epicurius <nneronin@student.hive.fi>
2021-12-10 19:49:36 +0200

    norm.sh

--- norm.sh
+++ norm.sh
@@ -1,2 +1,2 @@
-norminette-beta game/srcs/*.c game/inc/*.c get_path/*.c launcher/*.c libs/libft/*.c libs/better_libui/*.c libs/libtp/*.c libs/libpf/*.c libs/libgfx/*.c map_editor/*.c
-norminette-beta game/srcs/*.h game/inc/*.h get_path/*.h launcher/*.h libs/libft/*.h libs/better_libui/*.h libs/libtp/*.h libs/libpf/*.h libs/libgfx/*.h map_editor/*.h
+norminette game/srcs/*.c game/inc/*.c get_path/*.c launcher/*.c libs/libft/*.c libs/libui/*.c libs/libtp/*.c libs/libpf/*.c
+norminette game/srcs/*.h game/inc/*.h get_path/*.h launcher/*.h libs/libft/*.h libs/libui/*.h libs/libtp/*.h libs/libpf/*.h
