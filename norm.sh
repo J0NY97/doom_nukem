caf38b61d9d624b6f8b5ade165a3031c8520d2e9
Epicurius <nneronin@student.hive.fi>
2021-12-11 10:20:38 +0200

    updated norm.sh and game

--- norm.sh
+++ norm.sh
@@ -1,2 +1,20 @@
-norminette game/srcs/*.c game/inc/*.c get_path/*.c launcher/*.c libs/libft/*.c libs/libui/*.c libs/libtp/*.c libs/libpf/*.c
+norminette\
+ game/srcs/*.c\
+ game/srcs/BuyMenu/*.c\
+ game/srcs/CollisionDetection/*.c\
+ game/srcs/Draw/*.c\
+ game/srcs/EntityCoords/*.c\
+ game/srcs/Free/*.c\
+ game/srcs/Init/*.c\
+ game/srcs/Math/*.c\
+ game/srcs/ParseMap/*.c\
+ game/srcs/UI/*.c\
+ game/inc/*.c\
+ get_path/*.c\
+ launcher/*.c\
+ libs/libft/srcs/*.c\
+ libs/libui/*.c\
+ libs/libtp/srcs/*.c\
+ libs/libpf/srcs/*.c
+
 norminette game/srcs/*.h game/inc/*.h get_path/*.h launcher/*.h libs/libft/*.h libs/libui/*.h libs/libtp/*.h libs/libpf/*.h
