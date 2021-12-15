604411c2e1dcf37c7233ec4b61f07bff1f51d96e
Epicurius <nneronin@student.hive.fi>
2021-12-15 10:25:48 +0200

    Renamed SDL library paths

--- get_path/run_win.sh
+++ get_path/run_win.sh
@@ -1,3 +1,3 @@
-gcc -o find_path ./get_path/path.c -I./libs/libpf -L./libs/libpf -lpf -I./libs/libft -L./libs/libft -lft -I./libs/SDL/include -L./libs/SDL/lib -lSDL2;
+gcc -o find_path ./get_path/path.c -I./libs/libpf -L./libs/libpf -lpf -I./libs/libft -L./libs/libft -lft -I./libs/SDL_WIN/include -L./libs/SDL_WIN/lib -lSDL2;
 ./find_path;
 rm find_path;
