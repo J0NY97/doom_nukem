8fd041a0ce6c8234074c528d1d126326c2b2a222
Epicurius <nneronin@student.hive.fi>
2022-01-24 12:13:02 +0200

    Updated Submodules

--- norm.sh
+++ norm.sh
@@ -9,7 +9,6 @@ norminette game/srcs/Init/*.c | grep Error
 norminette game/srcs/Math/*.c | grep Error
 norminette game/srcs/ParseMap/*.c | grep Error
 norminette game/srcs/UI/*.c | grep Error
-norminette game/inc/*.c | grep Error
 
 norminette get_path/*.c | grep Error
 norminette launcher/*.c | grep Error
@@ -18,8 +17,7 @@ norminette libs/libui/*.c | grep Error
 norminette libs/libtp/srcs/*.c | grep Error
 norminette libs/libpf/srcs/*.c | grep Error
 
-#norminette	game/srcs/*.h | grep Error
-#norminette	game/inc/*.h | grep Error
+#norminette game/inc/*.h | grep Error
 #norminette	get_path/*.h | grep Error
 #norminette	launcher/*.h | grep Error
 #norminette	libs/libft/*.h | grep Error
