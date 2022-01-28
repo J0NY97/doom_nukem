6c84639007a40fea6e03646b794d1ab9b5140f82
Epicurius <nneronin@student.hive.fi>
2022-01-28 11:41:00 +0200

    Updated norm.sh

--- norm.sh
+++ norm.sh
@@ -1,19 +1,20 @@
 
 norminette game/srcs/*.c | grep Error
-norminette game/srcs/BuyMenu/*.c | grep Error
-norminette game/srcs/CollisionDetection/*.c | grep Error
-norminette game/srcs/Draw/*.c | grep Error
-norminette game/srcs/EntityCoords/*.c | grep Error
-norminette game/srcs/Free/*.c | grep Error
-norminette game/srcs/Init/*.c | grep Error
-norminette game/srcs/Math/*.c | grep Error
-norminette game/srcs/ParseMap/*.c | grep Error
-norminette game/srcs/UI/*.c | grep Error
+norminette game/srcs/UI | grep Error
+norminette game/srcs/Init | grep Error
+norminette game/srcs/Math | grep Error
+norminette game/srcs/Free | grep Error
+norminette game/srcs/Draw | grep Error
+norminette game/srcs/Events | grep Error
+norminette game/srcs/BuyMenu | grep Error
+norminette game/srcs/ParseMap | grep Error
+norminette game/srcs/EntityCoords | grep Error
+norminette game/srcs/CollisionDetection | grep Error
 
 norminette get_path/*.c | grep Error
 norminette launcher/*.c | grep Error
-norminette libs/libft/srcs/*.c | grep Error
 norminette libs/libui/*.c | grep Error
+norminette libs/libft/srcs/*.c | grep Error
 norminette libs/libtp/srcs/*.c | grep Error
 norminette libs/libpf/srcs/*.c | grep Error
 
