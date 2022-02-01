f4c57c1eca64329d94bf774b822e7ecb732c1c5d
Epicurius <nneronin@student.hive.fi>
2022-02-01 15:12:08 +0200

    screen size changes

--- norm.sh
+++ norm.sh
@@ -1,28 +1,27 @@
 
-norminette game/srcs/*.c | grep Error
-norminette game/srcs/UI | grep Error
-norminette game/srcs/Init | grep Error
-norminette game/srcs/Math | grep Error
-norminette game/srcs/Free | grep Error
-norminette game/srcs/Draw | grep Error
-norminette game/srcs/Events | grep Error
-norminette game/srcs/BuyMenu | grep Error
-norminette game/srcs/ParseMap | grep Error
-norminette game/srcs/EntityCoords | grep Error
-norminette game/srcs/CollisionDetection | grep Error
 
-norminette libs/libft/srcs/*.c | grep Error
-norminette libs/libtp/srcs/*.c | grep Error
-norminette libs/libpf/srcs/*.c | grep Error
+#	norminette get_path/*.h | grep Error
+#	norminette launcher/*.h | grep Error
+#	norminette libs/libui/*.h | grep Error
+#	norminette get_path/*.c | grep Error
+#	norminette launcher/*.c | grep Error
+#	norminette libs/libui/*.c | grep Error
 
-norminette game/inc/*.h | grep Error
-norminette libs/libft/*.h | grep Error
-norminette libs/libtp/*.h | grep Error
-norminette libs/libpf/*.h | grep Error
-
-#norminette	get_path/*.h | grep Error
-#norminette	launcher/*.h | grep Error
-#norminette	libs/libui/*.h | grep Error
-#norminette get_path/*.c | grep Error
-#norminette launcher/*.c | grep Error
-#norminette libs/libui/*.c | grep Error
+	norminette game/srcs/*.c | grep Error
+	norminette game/srcs/UI | grep Error
+	norminette game/srcs/Init | grep Error
+	norminette game/srcs/Math | grep Error
+	norminette game/srcs/Free | grep Error
+	norminette game/srcs/Draw | grep Error
+	norminette game/srcs/Events | grep Error
+	norminette game/srcs/BuyMenu | grep Error
+	norminette game/srcs/ParseMap | grep Error
+	norminette game/srcs/EntityCoords | grep Error
+	norminette game/srcs/CollisionDetection | grep Error
+	norminette libs/libft/srcs/*.c | grep Error
+	norminette libs/libtp/srcs/*.c | grep Error
+	norminette libs/libpf/srcs/*.c | grep Error
+	norminette game/inc/*.h | grep Error
+	norminette libs/libft/*.h | grep Error
+	norminette libs/libtp/*.h | grep Error
+	norminette libs/libpf/*.h | grep Error
